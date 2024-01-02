43074fa5833d44622bd93bce23b62c39da19c65b
daryl <livesaex@gmail.com>
2024-01-02 17:00:06 +0800

    feat: 渲染月统计收支

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,18 +1,45 @@
 import { DatePicker, NavBar } from 'antd-mobile'
 import './index.scss'
 import classNames from 'classnames'
-import { useState } from 'react'
+import {useMemo, useState} from 'react'
 import dayjs from 'dayjs'
+import { useSelector } from 'react-redux'
+import _ from 'lodash'
 
 const Month = () => {
+  // state
   const [visible, setVisible] = useState(false)
-  const [currentDate, setCurrentDate] = useState(() => dayjs(new Date()).format('YYYY | MM'))
+  const [currentDate, setCurrentDate] = useState(() => dayjs(new Date()).format('YYYY-MM'))
+  const [currentMonth, setCurrentMonth] = useState([])
+  const billList = useSelector(state => state.bill.billList)
 
+  // method
   const confirmFn = (date) => {
     setVisible(date)
-    const formatDate = dayjs(date).format('YYYY | MM')
+    const formatDate = dayjs(date).format('YYYY-MM')
     setCurrentDate(formatDate)
+    const curMonth = monthGroup[formatDate]
+    setCurrentMonth(curMonth ? curMonth : [])
   }
+  const plusMoneyFn = (monthBills, type) => {
+    return monthBills ? monthBills.filter(item => item.type === type).reduce((a, c) => a + c.money, 0) : 0
+  }
+
+  // hooks
+  // 对返回的账单数据进行按月分组。使用 memo 函数渲染过程中会缓存计算结果，只有在依赖项发生变化时，才重新计算结果。
+  const monthGroup = useMemo(() => {
+    return _.groupBy(billList,(item) => dayjs(item.date).format('YYYY-MM'))
+  }, [billList])
+  const monthResult = useMemo(() => {
+    // 支出、收入、结余
+    const pay = plusMoneyFn(currentMonth, 'pay')
+    const income = plusMoneyFn(currentMonth, 'income')
+    return {
+      pay,
+      income,
+      total: pay + income
+    }
+  }, [currentMonth])
 
   return (
     <div className="monthlyBill">
@@ -30,15 +57,15 @@ const Month = () => {
           {/* 统计区域 */}
           <div className="twoLineOverview">
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{monthResult.pay}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{8100}</span>
+              <span className="money">{monthResult.income}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{8000}</span>
+              <span className="money">{monthResult.total}</span>
               <span className="type">节余</span>
             </div>
           </div>
