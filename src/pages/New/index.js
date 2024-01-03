ea0d09641755ea8a4d2e565f111b09b23e2cc49b
daryl <livesaex@gmail.com>
2024-01-03 16:21:55 +0800

    feat: 记账页面搭建

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,7 +1,55 @@
+import { Button, DatePicker, Input, NavBar } from 'antd-mobile'
+import './index.scss'
+import classNames from 'classnames'
+import { useNavigate } from 'react-router-dom'
+import { billListData } from '@/utils/util'
+
 const New = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>
-      this is new
+    <div className="keepAccounts">
+      <NavBar className="nav" onBack={() => navigate(-1)}>记账</NavBar>
+      <div className="header">
+        <div className="kaType">
+          <Button className={classNames('selected')} shape="rounded">支出</Button>
+          <Button className={classNames('')} shape="rounded">收入</Button>
+        </div>
+        <div className="kaFormWrapper">
+          <div className="kaForm">
+            <div className="date">
+              <span className="icon"></span>
+              <span className="text">{'今天'}</span>
+              <DatePicker className="kaDate" title="记账日期" max={new Date()} />
+            </div>
+            <div className="kaInput">
+              <Input className="input" placeholder="0.00" type="number" />
+              <span className="iconYuan">¥</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="kaTypeList">
+        { billListData['pay'].map(item =>
+          <div className="kaType" key={item.type}>
+            <div className="title">{item.name}</div>
+            <div className="list">
+              { item.list.map(item => <div className={classNames('item','')} key={item.type}>
+                  <div className="icon">
+                    <span type={item.type} />
+                  </div>
+                  <div className="text">{item.name}</div>
+              </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="btns">
+        <Button className="btn save">保 存</Button>
+      </div>
     </div>
   )
 }
